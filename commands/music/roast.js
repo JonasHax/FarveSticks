190c62c51963fcc49b051fe9579c07d22a9c0f5c
Jonas Haxholm <jonashaxholm@hotmail.dk>
2021-09-28 19:36:27 +0200

    more phat roasts

--- commands/music/roast.js
+++ commands/music/roast.js
@@ -31,6 +31,21 @@ module.exports = {
       "du er så dum, at du falder over trådløst internet.",
       "du er så dum, at du ikke kan finde rundt i en telefonboks.",
       "du er så fed, at du får syet kjoler samme sted som Cirkus Benneweiss får syet cirkustelt.",
+      "jeg troede, jeg sagde farvel til dig, da jeg skyllede dig ud i toilettet i morges",
+      "selvfølgelig snakker jeg som en idiot. Ellers ville dui jo ikke kunne forstå mig",
+      "af og til undrer jeg mig over, om dit røvhul er jaloux over alt det lort, der kommer ud af din mund",
+      "jeg ville med glæder fornærme dig, men jeg er ikke sikker på, at jeg ville kunne gøre det lige så godt, som naturen allerede har gjort",
+      "du, min ven, skulle have været slugt",
+      "du ligner noget, jeg har tegnet med venstre hånd",
+      "et eller andet sted derude er der et træ, der producerer ilt, så du kan trække vejret. Jeg tror, at du skylder det en undskyldning",
+      "nogle folk fortjener bare en high five. I fjæset, Med en hammer",
+      "du er så grim, at du kommer ud med en lønseddel når du går igennem spøgelseshuset",
+      "jeg ville med glæder udfordre dig i en duel på viden, men jeg kan se, at du er ubevæbnet",
+      "så der var en tanke der strejfede dig? Den må have haft en lang og ensom tur",
+      "din mor er så fed at hvis hun stiller sig på vægten kan man se hendes telefonnummer",
+      "din mor har så mange vorter i hovedet, at hendes ansigt betyder grim på blindeskrift",
+      "når du mor går forbi TV'et, går jeg glip af hele Avatar",
+      "",
     ];
 
     var insultNumber = Math.random() * (insults.length - 0) + 1;
